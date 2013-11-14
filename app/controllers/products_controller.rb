2c77f7ab6d305b03c5930278e08d833d14298b22
jay-menorca <zai_menorca@yahoo.com>
2013-11-14 12:33:34 -0500

    added show product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,14 @@ require 'open-uri'
 class ProductsController < ApplicationController
 
 	def index
-		products_json = open('http://lcboapi.com/products').read
+		products_json = open('http://lcboapi.com/products?page=50').read
 		@products = JSON.parse(products_json)
 	end
 
+	def show
+		url = "http://lcboapi.com/products/" + params[:id]
+		product_json = open(url).read
+		@product = JSON.parse(product_json)
+	end
 
 end
